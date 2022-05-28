1c80fb2ca506f034a16460ced6c98cee81ec4bb7
imran farooq <imran_farooq7@hotmail.com>
2022-05-29 04:03:08 +0500

    Side structure created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,35 @@
-import './App.css'
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import "./App.css";
 const App = () => {
-	return <h1 className="underline text-3xl">App</h1>;
+	const activeMenu = true;
+	return (
+		<div>
+			<BrowserRouter>
+				<div className="flex relative dark:bg-main-dark-bg">
+					<div className="fixed right-4 bottom-4" style={{ zIndex: 1000 }}>
+						<TooltipComponent content="Settings" position="top">
+							<button
+								type="button"
+								className="text-4xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+								style={{ backgroundColor: "pink", borderRadius: "50%" }}>
+								<FiSettings />
+							</button>
+						</TooltipComponent>
+					</div>
+					{activeMenu ? (
+						<div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
+							Sidebar
+						</div>
+					) : (
+						<div>Sidebar w</div>
+					)}
+				</div>
+			</BrowserRouter>
+		</div>
+	);
 };
 
 export default App;

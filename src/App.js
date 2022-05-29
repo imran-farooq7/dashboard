f768b83016bcb227226070ec82bc486fe6483af4
imran farooq <imran_farooq7@hotmail.com>
2022-05-30 03:44:41 +0500

    Routes created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 const App = () => {
-	const activeMenu = true;
+	const activeMenu = false;
 	return (
 		<div>
 			<BrowserRouter>
@@ -24,8 +23,36 @@ const App = () => {
 							Sidebar
 						</div>
 					) : (
-						<div>Sidebar w</div>
+						<div className="w-0 dark:bg-secondary-dark-bg">Sidebar w</div>
 					)}
+					<div
+						className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
+							activeMenu ? "md:ml-72" : "flex-1"
+						}`}>
+						<div className="fixed md:static dark:bg-main-dark-bg navbar w-full">
+							Navbar
+						</div>
+					</div>
+					<div>
+						<Routes>
+							<Route path="/" element="Home" />
+							<Route path="/ecommerce" element="ecommerce" />
+							<Route path="/orders" element="orders" />
+							<Route path="/employees" element="employees" />
+							<Route path="/customers" element="customers" />
+							<Route path="/kanban" element="kanban" />
+							<Route path="/editor" element="editor" />
+							<Route path="/calendar" element="calendar" />
+							<Route path="/color-picker" element="colorpicker" />
+							<Route path="/line" element="line" />
+							<Route path="/area" element="area" />
+							<Route path="/bar" element="bar" />
+							<Route path="/financial" element="financial" />
+							<Route path="/colormapping" element="colormapping" />
+							<Route path="/pyramid" element="pyramid" />
+							<Route path="/stacked" element="stacked" />
+						</Routes>
+					</div>
 				</div>
 			</BrowserRouter>
 		</div>
